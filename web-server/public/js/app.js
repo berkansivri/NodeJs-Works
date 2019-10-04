6c2340d5420f49461d1114c81c187e13e1a1618b
berkansivri <berkansivri@gmail.com>
2019-10-04 11:56:04 +0300

    continue on work, setup for heroku

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,30 @@
-console.log("started");
+const getWeather = (address) => {
+  return fetch(`/weather?address=${address}`).then((response) => {
+    return response.json().then((data) => {
+      if (data.error) {
+        console.error(data.error)
+      } else {
+        console.log(data);
+      }
+      return data
+    })
+  })
+}
 
-fetch('http://localhost:3000/weather?address=istanbul').then((response) => {
-  response.json().then((data) => {
-    if(data.error) {
-      console.error(data.error);
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-one')
+const messageTwo = document.querySelector('#message-two')
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = search.value
+  getWeather(location).then((data) => {
+    if (data.error) {
+      messageOne.textContent = data.error
     } else {
-      console.log(data);
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
     }
   })
 })
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+
 const app = express()
+const port = process.env.PORT || 3000
 
 //setup handlebars engine and views and partials folder
 app.set('view engine', 'hbs')
@@ -88,6 +90,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port " +  port);
 })
