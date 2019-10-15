c2ab6a3c52fe2484223d7d09b5a12a33f84dfee2
berkansivri <berkansivri@gmail.com>
2019-10-15 11:21:11 +0300

    sending emails, .env configurations

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -4,11 +4,13 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
 const sharp = require('sharp')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body)
   try {
     await user.save()
+    sendWelcomeEmail(user.email, user.name)
     const token = await user.generateAuthToken()
     res.status(201).send({ user, token })
   } catch (error) {
@@ -72,6 +74,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
   try {
     await req.user.remove()
+    sendCancelationEmail(req.user.email, req.user.name)
     res.send()
   } catch (e) {
     res.status(500).send(e)
