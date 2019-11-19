a606059a37930e239edac7d962a3c7c2685c0f48
berkansivri <berkansivri@gmail.com>
2019-11-19 17:12:20 +0300

    chat-app continue

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -14,11 +15,13 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-  console.log("new ws connection")
-  
-  socket.emit('message', 'Welcome!')
 
-  socket.broadcast.emit('message', 'New user has joined!')
+  socket.on('join', ({ username, room }) => {
+    socket.join(room)
+
+     socket.emit('message', generateMessage('Welcome!'))
+     socket.broadcast.to(room).emit('message', generateMessage(`${username} has Joined!`))
+  })
 
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter()
@@ -27,12 +30,12 @@ io.on('connection', (socket) => {
       return callback('Profanity is not allowed!')
     }
 
-    io.emit('message', message)
+    io.emit('message', generateMessage(message))
     callback()
   })
 
   socket.on('sendLocation', (coords, callback) => {
-    socket.broadcast.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    socket.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
     callback()
   })
 
---
+++ chat-app/src/utils/users.js
@@ -0,0 +1,42 @@
+const users = []
+
+const addUser = ({ id, username, room }) => {
+  username = username.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+
+  if (!username || !room) {
+    return {
+      error: 'username and room are required!'
+    }
+  }
+  const existingUser = users.find(u => u.room === room && u.username === username)
+  if (existingUser) {
+    return {
+      error: 'username existing in this room!'
+    }
+  }
+
+  const user = { id, username, room }
+  users.push(user)
+  return { user }
+}
+
+const removeUser = (id) => {
+  const index = users.findIndex(u => u.id === id)
+  if(index > -1) users.splice(index, 1)[0]
+}
+
+const getUser = (id) => {
+  return users.find(u => u.id === id)
+}
+
+const getUsersInRoom = (room) => {
+  return users.filter(u => u.room === room)
+}
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+}
